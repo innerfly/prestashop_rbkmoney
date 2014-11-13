072f3895eda97608eac56184053d3d17edd0b588
innerfly <v.sukhoff@gmail.com>
2014-11-13 12:44:23 +0300

    initial 1.6.x

--- rbkmoney.php
+++ rbkmoney.php
@@ -1,100 +1,82 @@
 <?php
-/*
-* 2007-2012 PrestaShop
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Academic Free License (AFL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/afl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to license@prestashop.com so we can send you a copy immediately.
-*
-* DISCLAIMER
-*
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
-* versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
-*
-*  @author PrestaShop SA <contact@prestashop.com>
-*  @copyright  2007-2012 PrestaShop SA
-*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
-*  International Registered Trademark & Property of PrestaShop SA
-*/
 
-class Rbkmoney extends PaymentModule {
-  function __construct() {
-    $this->name = 'rbkmoney';
-    $this->tab = 'payments_gateways';
-    $this->version = '0.1';
-    $this->currencies = TRUE;
-    $this->currencies_mode = 'radio';
+class Rbkmoney extends PaymentModule
+{
+    function __construct()
+    {
+        $this->name = 'rbkmoney';
+        $this->tab = 'payments_gateways';
+        $this->version = '0.2';
+        $this->currencies = true;
+        $this->currencies_mode = 'radio';
 
-    parent::__construct(); /* The parent construct is required for translations */
+        parent::__construct(); /* The parent construct is required for translations */
 
-    $this->page = basename(__FILE__, '.php');
-    $this->displayName = $this->l('RBK Money payment system');
-    $this->description = $this->l('Accept payments by RBK Money payment system');
-  }
-
-  function install() {
-    parent::install();
-    Configuration::updateValue('RBKM_ESHOPID', '');
-    Configuration::updateValue('RBKM_PASS', '');
-    Configuration::updateValue('RBKM_DEBUG', 'false');
-    $this->registerHook('payment');
-    $this->registerHook('paymentReturn');
-    return TRUE;
-  }
-
-  function uninstall() {
-    if (!Configuration::deleteByName('RBKM_ESHOPID')
-      OR !Configuration::deleteByName('RBKM_PASS')
-      OR !Configuration::deleteByName('RBKM_DEBUG')
-      OR !parent::uninstall()
-    ) {
-      return FALSE;
-    }
-    return TRUE;
-  }
-
-  public function getContent() {
-    $this->output = "<h2>RBK Money</h2>";
-
-    if ($_POST['submit']) {
-      if (!empty($_POST['rbkm_eshopid'])) {
-        Configuration::updateValue('RBKM_ESHOPID', $_POST['rbkm_eshopid']);
-      }
-      if (!empty($_POST['rbkm_pass'])) {
-        Configuration::updateValue('RBKM_PASS', $_POST['rbkm_pass']);
-      }
-      if (!empty($_POST['rbkm_debug'])) {
-        Configuration::updateValue('RBKM_DEBUG', $_POST['rbkm_debug']);
-      }
-      $this->output .= '<div class="conf confirm"><img src="../img/admin/ok.gif" alt="' . $this->l('Confirmation') . '" />' . $this->l('Settings updated') . '</div>';
+        $this->page = basename(__FILE__, '.php');
+        $this->displayName = $this->l('RBK Money payment system');
+        $this->description = $this->l('Accept payments by RBK Money payment system');
     }
 
-    $this->displayFormSettings();
-
-    return $this->output;
-  }
-
-  public function displayFormSettings() {
-    $rbkm_eshopid = htmlentities(Configuration::get('RBKM_ESHOPID'), ENT_COMPAT, 'UTF-8');
-    $rbkm_pass = htmlentities(Configuration::get('RBKM_PASS'), ENT_COMPAT, 'UTF-8');
-
-    if (htmlentities(Configuration::get('RBKM_DEBUG'), ENT_COMPAT, 'UTF-8') == 'true') {
-      $on = 'checked="checked"';
-      $off = '';
-    }
-    else {
-      $on = '';
-      $off = 'checked="checked"';
+    function install()
+    {
+        parent::install();
+        Configuration::updateValue('RBKM_ESHOPID', '');
+        Configuration::updateValue('RBKM_PASS', '');
+        Configuration::updateValue('RBKM_DEBUG', 'false');
+        $this->registerHook('payment');
+        $this->registerHook('paymentReturn');
+        return true;
     }
 
-    $this->output .= '<form action="' . $_SERVER['REQUEST_URI'] . '" method="post">
+    function uninstall()
+    {
+        if (!Configuration::deleteByName('RBKM_ESHOPID') OR !Configuration::deleteByName('RBKM_PASS') OR !Configuration::deleteByName('RBKM_DEBUG') OR !parent::uninstall()
+        ) {
+            return false;
+        }
+        return true;
+    }
+
+    public function getContent()
+    {
+        $this->output = "<h2>RBK Money</h2>";
+
+        if ($_POST['submit']) {
+            if (!empty($_POST['rbkm_eshopid'])) {
+                Configuration::updateValue('RBKM_ESHOPID',  $_POST['rbkm_eshopid']);
+            }
+            if (!empty($_POST['rbkm_pass'])) {
+                Configuration::updateValue('RBKM_PASS', $_POST['rbkm_pass']);
+            }
+            if (!empty($_POST['rbkm_debug'])) {
+                Configuration::updateValue('RBKM_DEBUG', $_POST['rbkm_debug']);
+            }
+            $this->output .= '<div class="conf confirm"><img src="../img/admin/ok.gif" alt="' . $this->l('Confirmation') . '" />' . $this->l('Settings updated') . '</div>';
+        }
+
+        $this->displayFormSettings();
+
+        return $this->output;
+    }
+
+    public function displayFormSettings()
+    {
+        $rbkm_eshopid = htmlentities(Configuration::get('RBKM_ESHOPID'),
+          ENT_COMPAT, 'UTF-8');
+        $rbkm_pass = htmlentities(Configuration::get('RBKM_PASS'), ENT_COMPAT,
+          'UTF-8');
+
+        if (htmlentities(Configuration::get('RBKM_DEBUG'), ENT_COMPAT,
+            'UTF-8') == 'true'
+        ) {
+            $on = 'checked="checked"';
+            $off = '';
+        } else {
+            $on = '';
+            $off = 'checked="checked"';
+        }
+
+        $this->output .= '<form action="' . $_SERVER['REQUEST_URI'] . '" method="post">
 		<fieldset>
 			<legend><img src="../img/admin/contact.gif" />' . $this->l('Settings') . '</legend>
 			<label for="rbkm_eshopid">' . $this->l('RBK Money eshop ID') . '</label>
@@ -110,51 +92,83 @@ class Rbkmoney extends PaymentModule {
     <div class="margin-form"><input type="submit" name="submit" value="' . $this->l('Update settings') . '" class="button" /></div>
 		</fieldset>
 		</form>';
-  }
-
-
-  function hookPayment($params) {
-    global $smarty;
-
-    $address = new Address(intval($params['cart']->id_address_invoice));
-    $customer = new Customer(intval($params['cart']->id_customer));
-    $rbkm_eshopid = Configuration::get('RBKM_ESHOPID');
-    $currency = $this->getCurrency();
-
-    if (!Validate::isLoadedObject($address) OR !Validate::isLoadedObject($customer) OR !Validate::isLoadedObject($currency)) {
-      return $this->l('Error: (invalid address or customer)');
     }
 
-    $products = $params['cart']->getProducts();
-    $serviceName = '';
 
-    foreach ($products as $key => $product) {
-      $products[$key]['name'] = str_replace('"', '\'', $product['name']);
-      if (isset($product['attributes'])) {
-        $products[$key]['attributes'] = str_replace('"', '\'', $product['attributes']);
-      }
-      $products[$key]['name'] = htmlentities(utf8_decode($product['name']));
-      $serviceName .= $products[$key]['name'] . ' ';
+    function hookPayment($params)
+    {
+        $address = new Address(intval($params['cart']->id_address_invoice));
+        $customer = new Customer(intval($params['cart']->id_customer));
+        $eshopid = Configuration::get('RBKM_ESHOPID');
+        $secret_key = Configuration::get('RBKM_PASS');
+        $currency = $this->getCurrency();
+        $recipientAmount = number_format(Tools::convertPrice($params['cart']->getOrderTotal(true,
+              3), $currency), 2, '.', '');
+
+        if (!Validate::isLoadedObject($address) OR !Validate::isLoadedObject($customer) OR !Validate::isLoadedObject($currency)) {
+            return $this->l('Error: (invalid address or customer)');
+        }
+
+        $products = $params['cart']->getProducts();
+        $serviceName = '';
+        foreach ($products as $key => $product) {
+            $products[$key]['name'] = str_replace('"', '\'', $product['name']);
+            if (isset($product['attributes'])) {
+                $products[$key]['attributes'] = str_replace('"', '\'',
+                  $product['attributes']);
+            }
+            $products[$key]['name'] = htmlentities(utf8_decode($product['name']));
+            $serviceName .= $products[$key]['name'] . ' ';
+        }
+
+        // change currency 'RUB' to 'RUR'
+        $currency = ($this->getCurrency($params['cart']->id_currency)->iso_code == 'RUB') ? 'RUR' : $this->getCurrency($params['cart']->id_currency)->iso_code;
+
+        $hash_string = $eshopid . '::' . $recipientAmount . '::' . $currency . '::' . $customer->email . '::' . $serviceName . '::' . $params['cart']->id . '::::' . $secret_key;
+
+        $url = 'http://' . htmlspecialchars($_SERVER['HTTP_HOST'], ENT_COMPAT,
+            'UTF-8') . __PS_BASE_URI__ . 'index.php?controller=history';
+
+        global $smarty;
+
+        $smarty->assign(array(
+          'logo' => _MODULE_DIR_ . $this->name . '/rbkm_logo.png',
+//          'action' => _MODULE_DIR_ . $this->name . '/submit.php',
+          'action' => 'https://rbkmoney.ru/acceptpurchase.aspx',
+          'serviceName' => $serviceName,
+          'customer' => $customer,
+          'eshopId' => $eshopid,
+          'currency' => $currency,
+          'recipientAmount' => $recipientAmount,
+          'id_cart' => intval($params['cart']->id),
+          'hash' => md5($hash_string),
+          'successUrl' => $url . '&status=success',
+          'failUrl' => $url . '&status=fail',
+        ));
+
+        return $this->display(__FILE__, 'payment.tpl');
     }
-    if ($currency->iso_code == 'RUB') {
-      $currency->iso_code = 'RUR';
+
+    function hookPaymentReturn($params)
+    {
+        return $this->display(__FILE__, 'confirmation.tpl');
     }
-    $smarty->assign(array(
-      'serviceName' => $serviceName,
-      'customer' => $customer,
-      'eshopId' => $rbkm_eshopid,
-      'currency' => $currency,
-      'recipientAmount' => number_format(Tools::convertPrice($params['cart']->getOrderTotal(TRUE, 3), $currency), 2, '.', ''),
-      'id_cart' => intval($params['cart']->id),
-      'successUrl' => 'http://' . htmlspecialchars($_SERVER['HTTP_HOST'], ENT_COMPAT, 'UTF-8') . __PS_BASE_URI__ . 'order-confirmation.php?key=' . $customer->secure_key . '&id_cart=' . intval($params['cart']->id) . '&id_module=' . intval($this->id),
-      'failUrl' => 'http://' . htmlspecialchars($_SERVER['HTTP_HOST'], ENT_COMPAT, 'UTF-8') . __PS_BASE_URI__ . 'index.php',
-    ));
 
-    return $this->display(__FILE__, 'rbkmoney.tpl');
-  }
-
-  function hookPaymentReturn($params) {
-    return $this->display(__FILE__, 'confirmation.tpl');
-  }
+    public function validateOrder(
+      $id_cart,
+      $id_order_state,
+      $amount_paid,
+      $payment_method = 'Unknown',
+      $message = null,
+      $extra_vars = array(),
+      $currency_special = null,
+      $dont_touch_amount = false,
+      $secure_key = false,
+      Shop $shop = null
+    ) {
+        parent::validateOrder($id_cart, $id_order_state, $amount_paid,
+          $payment_method, $message, $extra_vars, $currency_special,
+          $dont_touch_amount, $secure_key, $shop);
+    }
 
 }
--- validation.php
+++ validation.php
@@ -1,90 +1,83 @@
 <?php
-/*
-* 2007-2012 PrestaShop
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Academic Free License (AFL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/afl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to license@prestashop.com so we can send you a copy immediately.
-*
-* DISCLAIMER
-*
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
-* versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
-*
-*  @author PrestaShop SA <contact@prestashop.com>
-*  @copyright  2007-2012 PrestaShop SA
-*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
-*  International Registered Trademark & Property of PrestaShop SA
-*/
 
 include(dirname(__FILE__) . '/../../config/config.inc.php');
 include(dirname(__FILE__) . '/rbkmoney.php');
 
-define('RBKMONEY_ALLOWED_IP', '89.111.188.128, 46.38.182.208, 46.38.182.209, 46.38.182.210, 89.111.188.129, 94.236.107.4, 94.236.107.1, 95.215.102.166');
+define('RBKMONEY_ALLOWED_IP', '89.111.188.128, 89.111.188.129, 94.236.107.4, 195.13.163.231, 46.38.182.209, 95.215.102.166, 46.38.182.208, 46.38.182.211');
+
 
 // check valid IP
 $allowed_ip = explode(',', RBKMONEY_ALLOWED_IP);
 $valid_ip = in_array($_SERVER['REMOTE_ADDR'], $allowed_ip);
-if (!$valid_ip) {
-  Logger::AddLog('[RBK Money error] Post request from not allowed IP:' . $_SERVER['REMOTE_ADDR'], 3, NULL, NULL, NULL, TRUE);
-  exit();
-}
-else {
-  header("HTTP/1.0 200 OK");
+if (!$valid_ip && isset($_POST['eshopId'])) {
+    Logger::AddLog('[RBK Money] Post request from not allowed IP:' . $_SERVER['REMOTE_ADDR'] . '. Status of order ID ' . stripcslashes($_POST['eshopId']) . ' not changed',
+      3, null, null, null, true);
+    exit();
+} else {
+    header("HTTP/1.0 200 OK");
 }
 
 if ($_POST) {
-  foreach ($_POST as $k => $v) {
-    $response[$k] = stripslashes($v);
-  }
-
-  $rbkmoney = new Rbkmoney();
-  $cart = new Cart(intval($response['orderId']));
-  $customer = new Customer(intval($cart->id_customer));
-
-  $eshopId = Configuration::get('RBKM_ESHOPID');
-  $secretKey = Configuration::get('RBKM_PASS');
-  $debug = Configuration::get('RBKM_DEBUG');
-
-  $currency = ($rbkmoney->getCurrency((int) $cart->id_currency)->iso_code == 'RUB') ? 'RUR' : $rbkmoney->getCurrency((int) $cart->id_currency)->iso_code;
-  $total = number_format(Tools::convertPrice($cart->getOrderTotal(TRUE, 4), $rbkmoney->getCurrency()), 2, '.', '');
-
-  $string = $eshopId . '::' . $response['orderId'] . '::' . $response['serviceName'] . '::' . $response['eshopAccount'] . '::' . $total . '::' . $currency . '::' . $response['paymentStatus'] . '::' . $response['userName'] . '::' . $response['userEmail'] . '::' . $response['paymentData'] . '::' . $secretKey;
-
-  $control_hash = md5($string);
-
-  if ($debug == '1') {
-    $rbkm_string = $response['eshopId'] . '::' . $response['orderId'] . '::' . $response['serviceName'] . '::' . $response['eshopAccount'] . '::' . $response['recipientAmount'] . '::' . $response['recipientCurrency'] . '::' . $response['paymentStatus'] . '::' . $response['userName'] . '::' . $response['userEmail'] . '::' . $response['paymentData'] . '::' . $secretKey;
-
-    Logger::AddLog('[RBK Money post] ' . $rbkm_string . ' hash:' . $response['hash'], 2, NULL, NULL, NULL, TRUE);
-    Logger::AddLog('[RBK Money control] ' . $string . ' hash:' . $control_hash, 2, NULL, NULL, NULL, TRUE);
-  }
-
-  if (($response['hash'] != $control_hash)) {
-    $err = "[RBK Money error] Hash mismatch. Control hash: {$control_hash} RBK Money hash:" . $response['hash'];
-    Logger::AddLog($err, 3, NULL, NULL, NULL, TRUE);
-    exit();
-  }
-  else {
-    if ($response['paymentStatus'] == '3') {
-      $rbkmoney->validateOrder((int)$cart->id, Configuration::get('PS_OS_BANKWIRE'), $cart->getOrderTotal(), 'rbkmoney', NULL, NULL, (int) $rbkmoney->getCurrency()->id, FALSE, $customer->secure_key);
-      $id_order = Order::getOrderByCartId(intval($response['orderId']));
-      $order = new Order($id_order);
-      $order->setCurrentState(Configuration::get('PS_OS_WS_PAYMENT'));
+    foreach ($_POST as $k => $v) {
+        $response[$k] = stripslashes($v);
     }
-    if ($response['paymentStatus'] == '5') {
-      $id_order = Order::getOrderByCartId(intval($response['orderId']));
-      $order = new Order($id_order);
-      $order->setCurrentState(Configuration::get('PS_OS_PAYMENT'));
+
+    $rbkmoney = new Rbkmoney();
+    $cart = new Cart(intval($response['orderId']));
+    $customer = new Customer(intval($cart->id_customer));
+
+    $eshopId = Configuration::get('RBKM_ESHOPID');
+    $secretKey = Configuration::get('RBKM_PASS');
+    $debug = Configuration::get('RBKM_DEBUG');
+
+    //    $currency = ($rbkmoney->getCurrency((int) $cart->id_currency)->iso_code == 'RUB') ? 'RUR' : $rbkmoney->getCurrency((int) $cart->id_currency)->iso_code;
+    $total = number_format(Tools::convertPrice($cart->getOrderTotal(true, 4),
+        $rbkmoney->getCurrency()), 2, '.', '');
+
+    $string = @implode('::', array(
+      $eshopId,
+      $response['orderId'],
+      $response['serviceName'],
+      $response['eshopAccount'],
+      $total,
+      $response['recipientCurrency'],
+      $response['paymentStatus'],
+      $response['userName'],
+      $response['userEmail'],
+      $response['paymentData'],
+      $secretKey
+    ));
+
+    $control_hash = md5($string);
+
+    if ($debug !== 'false') { // log response
+        foreach ($response as $k => $v) {
+            $query .= $k . "=" . $v . "&";
+        }
+        Logger::AddLog('[RBK Money] Received POST request: ' . $query, 2, null,
+          null, null, true);
     }
-  }
+
+    if (($response['hash'] != $control_hash)) {
+        $msg = "[RBK Money] Hash mismatch error. Control hash: {$control_hash}; RBK Money hash:" . $response['hash'];
+        Logger::AddLog($msg, 3, null, null, null, true);
+        exit();
+    } else {
+        if ($response['paymentStatus'] == '3') {
+            $rbkmoney->validateOrder((int) $cart->id,
+              Configuration::get('PS_OS_BANKWIRE'), $cart->getOrderTotal(),
+              'rbkmoney', null, null, (int) $rbkmoney->getCurrency()->id, false,
+              $customer->secure_key);
+            $id_order = Order::getOrderByCartId(intval($response['orderId']));
+            $order = new Order($id_order);
+            $order->setCurrentState(Configuration::get('PS_OS_WS_PAYMENT'));
+        }
+        if ($response['paymentStatus'] == '5') {
+            $id_order = Order::getOrderByCartId(intval($response['orderId']));
+            $order = new Order($id_order);
+            $order->setCurrentState(Configuration::get('PS_OS_PAYMENT'));
+        }
+    }
 }
 
 ?>
