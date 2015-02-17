f358f0d48c8a06fd18e8192d9a01ae83d551cdd8
bulinat <gornyak@gmail.com>
2015-02-17 15:55:19 +0300

    changed IP addresses

--- validation.php
+++ validation.php
@@ -3,7 +3,7 @@
 include(dirname(__FILE__) . '/../../config/config.inc.php');
 include(dirname(__FILE__) . '/rbkmoney.php');
 
-define('RBKMONEY_ALLOWED_IP', '89.111.188.128, 89.111.188.129, 94.236.107.4, 195.13.163.231, 46.38.182.209, 95.215.102.166, 46.38.182.208, 46.38.182.211');
+define('RBKMONEY_ALLOWED_IP', '89.111.188.128, 195.122.9.148');
 
 
 // check valid IP
